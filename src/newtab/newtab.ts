a410978c597d56262aacaac7c55f4aca88bd82f5
bluesky-o <blueskyco005@gmail.com>
2026-03-06 10:19:21 +0000

    added new tab to test the build

--- src/newtab/newtab.ts
+++ src/newtab/newtab.ts
@@ -1,1 +1,11 @@
-// 
+document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementsByClassName("btn-click")[0] as HTMLElement;
+    const counter = document.querySelector(".counter") as HTMLElement;
+
+    let count: number = 0;
+
+    btn.addEventListener("click", () => {
+        count += 1;
+        counter.textContent = count.toString();
+    });
+});
